eb0627281dcb63ac02bdbcdc6a1c949c0f8f4fbf
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-05-30 01:39:38 +0300

    fix: StickerFinder.Core

---
+++ StickerFinder.Core/Program.cs
@@ -0,0 +1,20 @@
+﻿using StickerFinder.Core.Generators;
+using StickerFinder.Core.Parsers;
+using StickerFinder.Core.Providers;
+using StickerFinder.Domain.Dto;
+
+var sticker = new StickerDto
+{
+    Name = "Sticker | Titan | Katowice 2014",
+    Price = 200000
+};
+
+string requestLink = new LinkGenerator(sticker).GetRequestLink();
+
+Console.WriteLine(requestLink);
+
+string rawData = await new RawDataProvider{RequestLink = requestLink}.GetRawDataAsync();
+/*Console.WriteLine(dataString);*/
+
+WeaponDto weapon = new RawDataParser(rawData).GetCheapestWeapon();
+Console.WriteLine(weapon);
